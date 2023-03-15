c5bacbc4878b1c71872144cf60bc59e9d123bfa2
RowanPl <rowanplooij@hotmail.com>
2023-03-15 18:59:19 +0100

    cleanSheet

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,11 +1,5 @@
 function Button({children, isDisabled, typeOfButton, onClick, Alert})
 {
 
-    return (
-        <button type={typeOfButton} disabled={isDisabled} onClick={onClick}>
-            { children }
-        </button>
-    )
-
 }
 export default Button;
